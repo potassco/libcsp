70e3736bc7bbfc746c622507cd1a33a5fa1b51c3
Benjamin Kaufmann <kaufmann@cs.uni-potsdam.de>
2017-07-13 12:53:56 +0200

    Fix type aliasing to make VC happy

--- liborder/order/types.h
+++ liborder/order/types.h
@@ -27,9 +27,9 @@
 
 namespace order
 {
-using uint64 = uint64_t;
-using int64 = int64_t;
+using ::uint64;
+using ::int64;
 
-using uint32 = uint32_t;
-using int32 = int32_t;
+using ::uint32;
+using ::int32;
 }
